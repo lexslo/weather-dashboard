c99a5988872e615841be6c528958412f6cdb5432
Sparkle Party <lexi.slovik@gmail.com>
2021-12-24 14:38:04 -0800

    pull data from API and update to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,80 @@
 // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=bc6a9ceb597185d2cfceed6b968a466f
 // lat and lon will be dynamically created and updated variables
 // exclude is an optional paramter to exclude specific data
+const apiKey = "bc6a9ceb597185d2cfceed6b968a466f";
+
+function kelvinToF (kelvin) {
+    var degreesF = ((kelvin - 273.15) * (9/5) + 32);
+    return degreesF;
+}
+
+function mpsToMph (mps) {
+    var mph = mps * 2.237;
+    return mph;
+}
+
+function getCityWeather (city) {
+    
+    apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                // console.log(data);
+                var temp = (Math.round(kelvinToF(data.main.temp)));
+                var wind = (Math.round(mpsToMph(data.wind.speed)));
+                var hum = ((data.main.humidity));
+                writeToPage(city, temp, wind, hum);
+                getUvi(data.coord.lat, data.coord.lon);
+            })
+        } else {
+            console.log("Error reaching open weather API");
+        }
+    });
+
+}
+
+function getUvi (lat, lon) {
+
+    apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=${apiKey}`;
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                //console.log(data);
+                console.log(data.current.uvi);
+                writeUvi(data.current.uvi);
+        })
+        } else {
+            console.log("error");
+        }
+    });
+}
+
+function writeToPage (city, temp, wind, hum) {
+    $("#city-name").text(city);
+    $("#temp").text(`Temp: ${temp}F`);
+    $("#wind").text(`Wind: ${wind}mph`);
+    $("#hum").text(`Humidity: ${hum}%`);
+}
+
+function writeUvi (uvi) {
+    $("#uvi").append("UV Index: ");
+    $("#uvi-status").append(uvi);
+
+    if (uvi < 5) {
+        $("#uvi-status").addClass("badge-success");
+    } else if (uvi > 5 && uvi < 10) {
+        $("#uvi-status").addClass("badge-warning");
+    } else if (uvi >= 10) {
+        $("#uvi-status").addClass("badge-danger");
+    }
+}
+
+$("#search-city").on("submit", function (event) {
+    event.preventDefault();
+
+    var city = $("#city").val();
+    getCityWeather(city);
+
+});
